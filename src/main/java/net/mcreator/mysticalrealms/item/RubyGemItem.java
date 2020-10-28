71d54b64d3e61b355c4db6b08f51de9cdfccbf4a
bdaly3411 <unconfigured@null.spigotmc.org>
2020-10-28 21:44:01 +0000

    Added ruby tools and armor

---
+++ src/main/java/net/mcreator/mysticalrealms/item/RubyGemItem.java
@@ -0,0 +1,46 @@
+
+package net.mcreator.mysticalrealms.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.mysticalrealms.MysticalrealmsModElements;
+
+@MysticalrealmsModElements.ModElement.Tag
+public class RubyGemItem extends MysticalrealmsModElements.ModElement {
+	@ObjectHolder("mysticalrealms:ruby_gem")
+	public static final Item block = null;
+	public RubyGemItem(MysticalrealmsModElements instance) {
+		super(instance, 22);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(ItemGroup.MISC).maxStackSize(64));
+			setRegistryName("ruby_gem");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+	}
+}
---
+++ src/main/java/net/mcreator/mysticalrealms/item/RubyHoeItem.java
@@ -0,0 +1,51 @@
+
+package net.mcreator.mysticalrealms.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.IItemTier;
+import net.minecraft.item.HoeItem;
+
+import net.mcreator.mysticalrealms.MysticalrealmsModElements;
+
+@MysticalrealmsModElements.ModElement.Tag
+public class RubyHoeItem extends MysticalrealmsModElements.ModElement {
+	@ObjectHolder("mysticalrealms:ruby_hoe")
+	public static final Item block = null;
+	public RubyHoeItem(MysticalrealmsModElements instance) {
+		super(instance, 32);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new HoeItem(new IItemTier() {
+			public int getMaxUses() {
+				return 1164;
+			}
+
+			public float getEfficiency() {
+				return 12f;
+			}
+
+			public float getAttackDamage() {
+				return 4f;
+			}
+
+			public int getHarvestLevel() {
+				return 6;
+			}
+
+			public int getEnchantability() {
+				return 42;
+			}
+
+			public Ingredient getRepairMaterial() {
+				return Ingredient.EMPTY;
+			}
+		}, -3f, new Item.Properties().group(ItemGroup.TOOLS)) {
+		}.setRegistryName("ruby_hoe"));
+	}
+}
