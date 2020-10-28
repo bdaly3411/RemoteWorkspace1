c2ad3cd5a201134832d17c39565b5e13a0058e4d
bdaly3411 <unconfigured@null.spigotmc.org>
2020-10-28 21:39:09 +0000

    Added tools and armor

---
+++ src/main/java/net/mcreator/mysticalrealms/item/SapphirePickaxeItem.java
@@ -0,0 +1,51 @@
+
+package net.mcreator.mysticalrealms.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.PickaxeItem;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.item.IItemTier;
+
+import net.mcreator.mysticalrealms.MysticalrealmsModElements;
+
+@MysticalrealmsModElements.ModElement.Tag
+public class SapphirePickaxeItem extends MysticalrealmsModElements.ModElement {
+	@ObjectHolder("mysticalrealms:sapphire_pickaxe")
+	public static final Item block = null;
+	public SapphirePickaxeItem(MysticalrealmsModElements instance) {
+		super(instance, 7);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new PickaxeItem(new IItemTier() {
+			public int getMaxUses() {
+				return 1164;
+			}
+
+			public float getEfficiency() {
+				return 12f;
+			}
+
+			public float getAttackDamage() {
+				return 4f;
+			}
+
+			public int getHarvestLevel() {
+				return 6;
+			}
+
+			public int getEnchantability() {
+				return 42;
+			}
+
+			public Ingredient getRepairMaterial() {
+				return Ingredient.EMPTY;
+			}
+		}, 1, -3f, new Item.Properties().group(ItemGroup.TOOLS)) {
+		}.setRegistryName("sapphire_pickaxe"));
+	}
+}
